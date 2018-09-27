a6e219d643cf290e40ccc130436f60a99bfcb5cf
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2018-09-27 10:44:16 -0400

    Enemy_C is implemented on the basic level
    TO BE DONE:
    When it actually shoots, make sure it inherits properly

---
+++ Assets/Scripts/Enemy/C/E_C.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_C : GenericEnemyScript {
+
+    // attributes
+    Vector2 position, tPosition, bulletVelocity;
+    float shotTimer;
+    List<GameObject> bullets;
+    GameObject firingPosition;
+
+    public float shotTimerMax, health;
+    public GameObject bulletPrefab;
+    public Vector2 speed, bulletSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        position = gameObject.transform.position;
+        velocity = speed;
+        bulletVelocity = bulletSpeed;
+        firingPosition = transform.GetChild(0).gameObject;
+        bullets = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Move();
+
+        // Firing
+        if (shotTimer == 0 || shotTimer >= shotTimerMax)
+        {
+            shotTimer = 0;
+            Fire();
+        }
+        else
+            shotTimer++;
+
+        // Bullet checking
+        BulletUpdate();
+	}
+
+    // movement method
+    void Move()
+    {
+        position += -velocity;
+        transform.position = position;
+    }
+
+    // firing method
+    void Fire()
+    {
+        GenerateBullet();
+        shotTimer++;
+    }
+
+    // creating a bullet
+    void GenerateBullet()
+    {
+        //Debug.Log("Firing Cork");
+        GameObject newBullet = Instantiate(bulletPrefab);
+        newBullet.transform.position = firingPosition.transform.position;
+        bullets.Add(newBullet);
+    }
+
+    // Updating bullet positions
+    void BulletUpdate()
+    {
+        foreach(GameObject bullet in bullets)
+        {
+            Vector2 newPosition = bullet.transform.position;
+            newPosition += -bulletVelocity;
+            bullet.transform.position = newPosition;
+        }
+
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            if (bullets[i].transform.position.y < -15 || bullets[i].transform.position.y > 15)
+            {
+                GameObject toDelete = bullets[i];
+                bullets.Remove(bullets[i]);
+                Destroy(toDelete);
+            }
+        }
+    }
+}
