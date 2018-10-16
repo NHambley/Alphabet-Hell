07f7df5dfd29c4a84a8bb8553cd5e3cd846b7929
alecw924 <43038028+alecw924@users.noreply.github.com>
2018-10-16 10:25:36 -0400

    b enemy moves

--- Assets/Scripts/Enemy/B/E_B.cs
+++ Assets/Scripts/Enemy/B/E_B.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class E_B : MonoBehaviour {
+public class E_B : GenericEnemyScript {
+    Vector3 position, tPosition;
+    float shieldHealth, damageTaken;
+    public Vector3 speed;
+    SceneManagerScript sceneManager;
+    public override void OnHit()
+    {
+        throw new System.NotImplementedException();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    // Use this for initialization
+    void Start () {
+        position = gameObject.transform.position;
+        velocity = speed;
+        sceneManager = GameObject.Find("SceneManager").GetComponent<SceneManagerScript>();
+    }
+    void Move()
+    {
+        position += -velocity;
+        transform.position = position;
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        Move();
 	}
 }
