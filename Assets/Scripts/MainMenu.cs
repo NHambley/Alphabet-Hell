778e50d8c3de870d375340bbf133cb9057605440
axw1799 <axw1799@IGM2350-22>
2018-10-21 17:59:16 -0400

    made a button but doesn't work cuz test scene is fucked

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour {
+
+    // Use this for initialization
+    public UnityEngine.UI.Button buttonComponent;
+    void Start () {
+        buttonComponent.onClick.AddListener(HandleClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("gotinhere");
+            Application.Quit();
+            Debug.Log("quit");
+        }
+            
+	}
+
+   //private void OnMouseUp()
+   //{
+   //    Debug.Log("gotinhere1");
+   //    SceneManager.LoadScene("TestScene");
+   //}
+
+    public void HandleClick()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+}
