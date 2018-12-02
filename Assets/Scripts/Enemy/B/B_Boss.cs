15fc21d4b8287c7d705d0458faa17ba1b5f44370
Alec Walker (RIT Student) <axw1799@ad.rit.edu>
2018-12-02 14:52:05 -0500

    f

---
+++ Assets/Scripts/Enemy/B/B_Boss.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_Boss : GenericBossScript {
+
+    Transform[] children;
+
+    [SerializeField]
+    GameObject bullet;
+    public GameObject obstacle;
+
+    float bTimer = 1.5f; // to keep track of when to fire another bullet
+    float timerTrack = 1.5f;
+    float obstacleTimerTrack = 4.0f;
+    float bObstacleTimer = 4.0f;
+    Vector3 obstaclePos;
+    // Use this for initialization
+    void Start()
+    {
+        obstaclePos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attacking();
+        spawnObstacle();
+    }
+
+    // rotate each frame by a set amount
+
+    void Attacking()
+    {
+        // subtract time from the timer to fire bullets
+        timerTrack -= Time.deltaTime;
+        if (timerTrack <= 0)
+        {
+            // instantiate a new bullet
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            //Debug.Log("got here");
+            timerTrack = bTimer;
+        }
+    }
+
+    void spawnObstacle()
+    {
+        obstaclePos.x = Random.Range(-3.0f, 3.5f);
+
+        obstacleTimerTrack -= Time.deltaTime;
+        if (obstacleTimerTrack <= 0)
+        {
+            // instantiate a new bullet
+            Instantiate(obstacle, obstaclePos, Quaternion.identity);
+            //Debug.Log("got here");
+            obstacleTimerTrack = bObstacleTimer;
+        }
+    }
+
+    public override void OnHit(Vector3 pos)
+    {
+        Health -= 2;
+    }
+}
---
+++ Assets/Scripts/Enemy/J/E_JEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_JEnemy:GenericEnemyScript
+{
+
+    public GameObject bulletPrefab;
+    SceneManagerScript manager;
+    float lastShootTime = -10.0f;
+
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.Find("SceneManager").GetComponent<SceneManagerScript>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - lastShootTime > 3.0f && gameObject.GetComponent<Renderer>().isVisible)
+        {
+            Shoot();
+            lastShootTime = Time.time;
+        }
+	}
+
+    public void Shoot()
+    {
+        GameObject newBullet1 = Instantiate(bulletPrefab);
+        newBullet1.transform.position = new Vector3(gameObject.transform.position.x,gameObject.transform.position.y - gameObject.GetComponent<SpriteRenderer>().bounds.extents.y, gameObject.transform.position.z - 1);
+        newBullet1.GetComponent<E_JBullet>().InitializeBullet(new Vector3(0f, -0.3f, 0f), new Vector3(0, 0, 0), gameObject);
+        manager.AddEnemyBullet(newBullet1);
+    }
+
+    public override void OnHit(Vector3 pos)
+    {
+        gameObject.GetComponent<ParticleGenerator>().GenerateParticles(SPRITE.ROCK, 3, pos, new Vector3(0.0f, 0.1f, 0.0f), new Vector3(0.2f, 0.2f, 0.2f), 90, 0.5f, -0.5f);
+    }
+}
