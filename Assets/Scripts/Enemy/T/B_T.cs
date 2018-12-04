61e2bbd0da7887c9cd413b90eb3b91a97bc6b8fd
NHambley <nhambley6@gmail.com>
2018-12-04 09:40:35 -0500

    Added in Boss T stuff

---
+++ Assets/Scripts/Enemy/T/B_T.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_T : GenericBossScript
+{
+    public override void OnHit(Vector3 pos)
+    {
+        Health -= 10;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        Health = 250;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
