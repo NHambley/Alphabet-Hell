46c9959916535e73eb74b89dee9033eff8b530c7
alecw924 <43038028+alecw924@users.noreply.github.com>
2018-12-06 10:29:05 -0500

    gdgfsj

--- Assets/Scripts/Enemy/V/VBoss.cs
+++ Assets/Scripts/Enemy/V/VBoss.cs
@@ -3,71 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VBoss : GenericBossScript {
-    Transform[] children;
-
-    [SerializeField]
-    GameObject bullet;
-    public GameObject bossBullet;
+    Vector3 position, tPosition;
+    Vector2 ePosition; // positino of this gameobject
+    float damageTaken;
+    public Vector2 speed;
+    SceneManagerScript sceneManager;
 
     float bTimer = 1.5f; // to keep track of when to fire another bullet
     float timerTrack = 1.5f;
-    float obstacleTimerTrack = 4.0f;
-    float bObstacleTimer = 4.0f;
-    Vector3 bossBulletPos;
-    Vector3 bossBulletPos1;
-    SceneManagerScript sM;
+
+
+
+
+    // get a reference to the state manager
+    [SerializeField]
+    bool state;
+
+    [SerializeField]
+    GameObject bullet;
+
+    Camera cam;
+    public override void OnHit(Vector3 hit)
+    {
+        Health -= 2;
+    }
+
     // Use this for initialization
     void Start()
     {
-        bossBulletPos = transform.position;
-        bossBulletPos.x = transform.position.x - 1;
-        bossBulletPos1 = transform.position;
-        bossBulletPos1.x = transform.position.x + 1;
-        sM = GameObject.FindGameObjectWithTag("GameController").GetComponent<SceneManagerScript>();
-
+        position = gameObject.transform.position;
+        velocity = speed;
+        sceneManager = GameObject.Find("SceneManager").GetComponent<SceneManagerScript>();
+        Health = 100;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Attacking();
-        bossAttack();
-    }
 
-    // rotate each frame by a set amount
+
+        
+        Attacking();
+
+    }
 
     void Attacking()
     {
+        // using the position that is set in the resetting method (or if it is the first attacking run of this enemy the one set in Start)have the enemy move down in some form towards that position 
+
         // subtract time from the timer to fire bullets
         timerTrack -= Time.deltaTime;
         if (timerTrack <= 0)
         {
+
             // instantiate a new bullet
-            sM.AddEnemyBullet(Instantiate(bullet, transform.position, Quaternion.identity));
+            GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            newBullet.GetComponent<VBossBullet>().dirOfBullet = 1;
+
+            GameObject newBullet1 = Instantiate(bullet, transform.position, Quaternion.identity);
+            newBullet1.GetComponent<VBossBullet>().dirOfBullet = 2;
+
+            GameObject newBullet2 = Instantiate(bullet, transform.position, Quaternion.identity);
+            newBullet.GetComponent<VBossBullet>().dirOfBullet = 3;
+            
+            GameObject newBullet3 = Instantiate(bullet, transform.position, Quaternion.identity);
+            newBullet1.GetComponent<VBossBullet>().dirOfBullet = 4;
+
+            sceneManager.AddEnemyBullet(newBullet);
+            sceneManager.AddEnemyBullet(newBullet1);
+            sceneManager.AddEnemyBullet(newBullet2);
+            sceneManager.AddEnemyBullet(newBullet3);
             //Debug.Log("got here");
             timerTrack = bTimer;
         }
-    }
-
-    void bossAttack()
-    {
 
 
-        obstacleTimerTrack -= Time.deltaTime;
-        if (obstacleTimerTrack <= 0)
-        {
-            // instantiate a new bullet
-            sM.AddEnemyBullet(Instantiate(bossBullet, bossBulletPos, Quaternion.identity));
-            sM.AddEnemyBullet(Instantiate(bossBullet, bossBulletPos1, Quaternion.identity));
 
-            //Debug.Log("got here");
-            obstacleTimerTrack = bObstacleTimer;
-        }
-    }
-
-    public override void OnHit(Vector3 pos)
-    {
-        Health -= 2;
     }
 }
---
+++ Assets/Scripts/Enemy/V/VBossBullet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VBossBullet : GenericBulletScript {
+
+    float speed;
+    Vector2 position;
+    Vector2 movement;
+    SceneManagerScript sM;
+    int leftOrRight;
+    int lengthOfArc;
+    int lengthOfArc1;
+    GameObject player;
+    public V_Enemy vScript;
+    public int dirOfBullet;
+    // Use this for initialization
+    void Start()
+    {
+        speed = 700;
+        position = transform.position;
+        movement = Vector2.down;
+        player = GameObject.FindGameObjectWithTag("Player");
+        sM = GameObject.FindGameObjectWithTag("GameController").GetComponent<SceneManagerScript>();
+        if (dirOfBullet == 1)
+            transform.Rotate(Vector3.forward * -65);
+        if (dirOfBullet == 2)
+            transform.Rotate(Vector3.forward * -25);
+        if (dirOfBullet == 3)
+            transform.Rotate(Vector3.forward * -85);
+        if (dirOfBullet == 4)
+            transform.Rotate(Vector3.forward * -5);
+        //Debug.Log(leftOrRight);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dirOfBullet == 1)
+        { 
+            position.x -= .01f;
+        }
+        if (dirOfBullet == 3)
+        {
+            position.x -= .02f;
+        }
+        if (dirOfBullet == 2 )
+        {
+            position.x += .01f;
+        }
+        if (dirOfBullet == 4)
+        {
+            position.x += .02f;
+        }
+
+        position.y -= .03f;
+
+        transform.position = position;
+    }
+}
