b368cc8771635ea94d43d2f7d1ff4bb202ce20dc
NHambley <nhambley6@gmail.com>
2018-12-08 22:46:46 -0500

    Started work on the H boss and updated the H sprites

---
+++ Assets/Scripts/Enemy/H/B_H.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_H : MonoBehaviour
+{
+    public GameObject[] firepoints;
+
+    public GameObject bullet;
+
+    float fpTimer = 1.0f;// left and right clocks
+    float fpTimer2 = 1.5f;// center, more powerful clock
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
